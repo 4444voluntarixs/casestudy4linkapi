39fb1c042e4b98797aabc1cce65bd0c5d9c2b31d
Victor F <mail@victorf.ch>
2017-08-16 02:58:25 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Caso de Estudo para LinkApi
+============================
+
+* [Estudo da aderência da solução no novo mercado](https://www.evernote.com/shard/s300/sh/31e3b650-6c21-48f3-bc31-a8c1bc239d8d/bd4975b4cd8acd6ab5c5f68f85dc7505)
---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Estudo de Caso - LinkApi</title>
+</head>    
+    
+<body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo 'Hello world from Cloud9!';
+
+?>
+</body>
+</html>
